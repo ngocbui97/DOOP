101000a0c6007227314a6b0d92fbf9a14a262fd8
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2018-12-04 22:22:31 +0700

    update Common

---
+++ Common/Common/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    class Connection
+    {
+        protected string _connectionString { get; set; }
+
+        public abstract void Open();
+        public abstract void Close();
+        public abstract Where<T> Select<T>() where T : new();
+        public abstract int Insert<T>(T obj) where T : new();
+        public abstract int Update<T>(T obj) where T : new();
+        public abstract int Delete<T>(T obj) where T : new();
+        public abstract List<T> ExecuteQuery<T>(string query) where T : new();
+        public abstract List<T> ExecuteQueryWithOutRelationship<T>(string query) where T : new();
+        public abstract int ExecuteNonQuery(string query);
+    }
+}
