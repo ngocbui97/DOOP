3e26d784ecabb8b47898506c578a37ffbc751f6d
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2018-12-04 22:19:46 +0700

    up Common

---
+++ Attribute/Common/QueryRun.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+
+    public interface IQuery
+    {
+        List<T> ExecuteQuery<T>() where T : new();
+        List<T> ExecuteQueryWithOutRelationship<T>() where T : new();
+        int ExecuteNonQuery();
+    }
+
+    public interface RunQuery<T> where T : new()
+    {
+        List<T> Run();
+    }
+
+}
